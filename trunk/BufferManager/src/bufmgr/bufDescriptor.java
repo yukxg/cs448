b4e423303ca5d92e3d8d4b29ad71794a1aa5e6a0
michaelrobtemp@gmail.com <michaelrobtemp@gmail.com@27b30fb7-0727-6476-5e9b-203574951ffe>
2013-03-20 21:07:26 +0000

    buffer descriptor

---
+++ trunk/BufferManager/src/bufmgr/bufDescriptor.java
@@ -0,0 +1,16 @@
+package bufmgr;
+
+import global.PageId;
+
+public class bufDescriptor {
+
+	private int pin_count;
+	private PageId page_number;
+	private boolean dirtybit;
+	
+	public bufDescriptor()
+	{
+		;
+	}
+	
+}
