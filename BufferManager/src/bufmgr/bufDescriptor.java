825f917659b0fb013266948550178c64de556135
michaelrobtemp <michaelrobtemp@gmail.com>
2013-03-21 03:32:48 +0000

    need to add methods when necessary

--- BufferManager/src/bufmgr/bufDescriptor.java
+++ BufferManager/src/bufmgr/bufDescriptor.java
@@ -10,7 +10,16 @@ public class bufDescriptor {
 	
 	public bufDescriptor()
 	{
-		;
+		pin_count = 0;
+		page_number = new PageId();
+		//how to get the unique pageid
+		dirtybit = false;
 	}
 	
+	//Accessors
+	
+	
+	//Mutators
+	
+	//TODO
 }
