233aba0bcf151c1cdf9db27c9b6f6e09e660b739
chenyangqu@gmail.com <chenyangqu@gmail.com@27b30fb7-0727-6476-5e9b-203574951ffe>
2013-03-22 00:17:32 +0000

    todo: unpin new page

--- trunk/BufferManager/src/bufmgr/HashTable.java
+++ trunk/BufferManager/src/bufmgr/HashTable.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 class node {
 	int page;
 	int frame;
+
 	public node(int page, int frame) {
 		this.page = page;
 		this.frame = frame;
@@ -18,31 +19,43 @@ public class HashTable {
 	public HashTable(int size) {
 		this.size = size;
 		table = new ArrayList[size];
+		for (int i = 0; i < table.length; i++)
+			table[i] = new ArrayList<node>();
 	}
 
-	public void addpage(int page,int frame){
-		node temp=new node(page,frame);
-		ArrayList index=table[(20*page+10)%size];
+	public void addpage(int page, int frame) {
+		node temp = new node(page, frame);
+		ArrayList index = table[(20 * page + 10) % size];
 		index.add(temp);
 	}
-	public int getframe(int page){
-		ArrayList index=table[(20*page+10)%size];
+
+	public int getframe(int page) {
+		ArrayList index = table[(20 * page + 10) % size];
 		node temp;
-		for(int i=0;i<index.size();i++){
-			temp=(node) index.get(i);
-			if(temp.page==page)
+		// System.out.println("in hash table"+index.size());
+		if (index == null)
+			return -1;
+		for (int i = 0; i < index.size(); i++) {
+			temp = (node) index.get(i);
+			if (temp.page == page)
 				return temp.frame;
 		}
 		return -1;
 	}
-	public void delete(int page){
-		ArrayList index=table[(20*page+10)%size];
+
+	public void delete(int page) {
+		// System.out.println("in hash table"+page);
+		// System.out.println("in hash table "+(20 * page + 10) % size);
+		ArrayList index = table[(20 * page + 10) % size];
 		node temp;
-		for(int i=0;i<index.size();i++){
-			temp=(node) index.get(i);
-			if(temp.page==page)
+		for (int i = 0; i < index.size(); i++) {
+			temp = (node) index.get(i);
+			if (temp.page == page)
 				index.remove(i);
 		}
-		
+	}
+
+	public ArrayList<node>[] getAllPages() {
+		return table;
 	}
 }
