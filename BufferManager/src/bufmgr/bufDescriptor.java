3d04cbe80eb5b489bd1806514ca1de5b5569d060
chenyangqu <chenyangqu@gmail.com>
2013-03-22 00:45:30 +0000

    bufferdesc

--- BufferManager/src/bufmgr/bufDescriptor.java
+++ BufferManager/src/bufmgr/bufDescriptor.java
@@ -7,42 +7,50 @@ public class bufDescriptor {
 	private int pin_count;
 	private PageId page_number;
 	private boolean dirtybit;
-	
-	public bufDescriptor()
-	{
+
+	public bufDescriptor() {
 		pin_count = 0;
 		page_number = new PageId();
-		//how to get the unique pageid
+		// how to get the unique pageid
 		dirtybit = false;
 	}
-	
-	//Accessors
-	public int get_pin_count()
-	{
+
+	// Accessors
+	public int get_pin_count() {
 		return pin_count;
 	}
-	
-	public PageId get_page_number()
-	{
+
+	public PageId get_page_number() {
 		return page_number;
 	}
-	
-	public boolean get_dirtybit()
-	{
+
+	public boolean get_dirtybit() {
 		return dirtybit;
 	}
-	
-	//Mutators
-	public void increase_pin_count()
-	{
+
+	public boolean isDirty() {
+		return dirtybit;
+	}
+
+	// Mutators
+	public void increase_pin_count() {
 		pin_count++;
 	}
-	
-	public void decrease_pin_count()
-	{
+
+	public void decrease_pin_count() {
 		pin_count--;
 	}
-	
-	
-	//TODO
+
+	public void setPage(int pageid) {
+		page_number.pid = pageid;
+	}
+
+	public void setPincount(int pin) {
+		pin_count = pin;
+	}
+
+	public void setdirty(boolean dirty) {
+		dirtybit = dirty;
+	}
+	// TODO
 }
