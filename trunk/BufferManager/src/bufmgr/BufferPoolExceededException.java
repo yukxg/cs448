755b21177e4d2435579591b5fda70ab06c330d13
michaelrobtemp@gmail.com <michaelrobtemp@gmail.com@27b30fb7-0727-6476-5e9b-203574951ffe>
2013-03-25 21:27:55 +0000

    new exception

---
+++ trunk/BufferManager/src/bufmgr/BufferPoolExceededException.java
@@ -0,0 +1,12 @@
+package bufmgr;
+
+import chainexception.ChainException;
+
+public class BufferPoolExceededException extends ChainException {
+
+	public BufferPoolExceededException(Exception e, String name)
+	{
+		super(e, name);
+	}
+	
+}
