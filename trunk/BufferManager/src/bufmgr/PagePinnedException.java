f9d52a3db1ec92a09b309a139d960eb4c57171bb
chenyangqu@gmail.com <chenyangqu@gmail.com@27b30fb7-0727-6476-5e9b-203574951ffe>
2013-03-25 22:06:43 +0000

    exception

---
+++ trunk/BufferManager/src/bufmgr/PagePinnedException.java
@@ -0,0 +1,9 @@
+package bufmgr;
+
+import chainexception.ChainException;
+
+public class PagePinnedException extends ChainException {
+	public PagePinnedException(Exception e, String name) {
+		super(e, name);
+	}
+}
