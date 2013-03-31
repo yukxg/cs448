e5725c9efcdd8c095f2627a709d80d7f27caeda1
michaelrobtemp <michaelrobtemp@gmail.com>
2013-03-31 07:20:34 +0000

    new default tuple class =P

--- HeapFile/src/heap/Tuple.java
+++ HeapFile/src/heap/Tuple.java
@@ -1,23 +1,73 @@
 package heap;
 
-public class Tuple {
+import global.GlobalConst;
 
-	public Tuple(byte[] byteArray, int i, int recLength) {
-		// TODO Auto-generated constructor stub
-	}
+public class Tuple implements GlobalConst {
+	public static final int max_size = PAGE_SIZE;
+	public byte[] data; // store the data
+	private int tuple_offset;
+	private int tuple_length;
+	private short fldCnt;
 
 	public Tuple() {
-		// TODO Auto-generated constructor stub
+		data = new byte[max_size];
+		tuple_offset = 0;
+		tuple_length = max_size;
 	}
 
-	public int getLength() {
-		// TODO Auto-generated method stub
-		return 0;
+	public Tuple(byte[] atupl) {
+		data = atupl;
+		tuple_offset = 0;
+		tuple_length = atupl.length;
+	}
+
+	/**
+	 * * construction function * * @param atuple * @param offset * @param length
+	 */
+	public Tuple(byte[] atuple, int offset, int length) {
+		data = atuple;
+		tuple_offset = offset;
+		tuple_length = length;
+	}
+
+	/** * * @param size */
+	public Tuple(int size) {
+		data = new byte[size];
+		tuple_offset = 0;
+		this.tuple_length = size;
+	}
+
+	/**
+	 * * Copy a tuple to the current tuple position you must make sure the tuple
+	 * * lengths must be equal * * @param fromTuple * the tuple being copied
+	 */
+	public void tupleCopy(Tuple fromTuple) {
+		data = fromTuple.getTupleByteArray();
+		tuple_length = fromTuple.getLength();
 	}
 
 	public byte[] getTupleByteArray() {
-		// TODO Auto-generated method stub
-		return null;
+		return data;
+	}
+
+	public int getLength() {
+		return tuple_length;
+	}
+
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+
+	public void setlength(int tuple_length) {
+		this.tuple_length = tuple_length;
+	}
+
+	public int getTuple_offset() {
+		return tuple_offset;
+	}
+
+	public void setTuple_offset(int tuple_offset) {
+		this.tuple_offset = tuple_offset;
 	}
 
 }
