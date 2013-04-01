8ff0c3a382b95bd36ee99ada61ffc371254225fc
michaelrobtemp@gmail.com <michaelrobtemp@gmail.com@27b30fb7-0727-6476-5e9b-203574951ffe>
2013-04-01 20:05:20 +0000

    first commit of scan version two

--- trunk/HeapFile/src/heap/HeapScan.java
+++ trunk/HeapFile/src/heap/HeapScan.java
@@ -1,6 +1,10 @@
 package heap;
 
+import java.util.Iterator;
+
 import chainexception.ChainException;
+import global.Minibase;
+import global.PageId;
 import global.RID;
 
 public class HeapScan {
@@ -12,15 +16,38 @@ public class HeapScan {
 	* iterator fields.
 
 	*/
+	
+	private HeapFile hf;
+	private HFPage current;
+	
+	private RID crid;
+	private Iterator<PageId> it;
 
 	protected HeapScan(HeapFile hf)
 	{
+		
+		this.hf = hf;
+		it = hf.iterator();
+		PageId currentId = it.next();
+		current = new HFPage();
+		
+		Minibase.BufferManager.pinPage(currentId, current, false);
+		crid = current.firstRecord();
 		
 	}
 
 
 	protected void finalize() throws Throwable
 	{
+		if(current != null)
+		{
+			Minibase.BufferManager.unpinPage(current.getCurPage(), false);
+		}
+		
+		current = null;
+		
+		crid = null;
+		it = null;
 		
 	}
 
@@ -32,6 +59,15 @@ public class HeapScan {
 	public void close() throws ChainException  //TODO
 	{
 		
+		try {
+			finalize();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		hf = null;
+		
 	}
 
 
@@ -42,7 +78,7 @@ public class HeapScan {
 	*/
 	public boolean hasNext()
 	{
-		return false;
+		return it.hasNext();
 		
 	}
 
@@ -57,9 +93,25 @@ public class HeapScan {
 	* @throws IllegalStateException if the scan has no more elements
 
 	*/
-	public Tuple getNext(RID rid)
+	public Tuple getNext(RID rid) throws IllegalStateException
 	{
-		return null;
+		
+		if(crid == null && it.hasNext())
+		{
+			Minibase.BufferManager.unpinPage(current.getCurPage(), false);
+			PageId temp = it.next();
+			
+			Minibase.BufferManager.pinPage(temp, current, false);
+			crid = current.firstRecord();
+		}
+		if(crid != null)
+		{
+			rid.copyRID(crid);
+			crid = current.nextRecord(crid);
+			return new Tuple(current.selectRecord(rid));
+		}
+		
+		throw new IllegalStateException();
 		
 	}
 	
