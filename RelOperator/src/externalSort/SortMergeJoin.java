3a7724f13ae4ad5e9889fc690d26b96eaddf2e8a
chenyangqu <chenyangqu@gmail.com>
2013-04-18 23:00:07 +0000

    create the class

---
+++ RelOperator/src/externalSort/SortMergeJoin.java
@@ -0,0 +1,20 @@
+package externalSort;
+
+import relop.FileScan;
+
+public class SortMergeJoin {
+
+	public SortMergeJoin(FileScan fileScan, FileScan fileScan2, int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SortMergeJoin(SortMergeJoin join1, FileScan fileScan2, int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
