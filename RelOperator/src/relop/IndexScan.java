56626e3c91c74c6f4b08af476e662368000fbe18
chenyangqu <chenyangqu@gmail.com>
2013-04-21 22:57:41 +0000

    part 1

--- RelOperator/src/relop/IndexScan.java
+++ RelOperator/src/relop/IndexScan.java
@@ -1,7 +1,9 @@
 package relop;
 
+import global.RID;
 import global.SearchKey;
 import heap.HeapFile;
+import index.BucketScan;
 import index.HashIndex;
 
 /**
@@ -12,8 +14,18 @@ public class IndexScan extends Iterator {
   /**
    * Constructs an index scan, given the hash index and schema.
    */
-  public IndexScan(Schema schema, HashIndex index, HeapFile file) {
-    throw new UnsupportedOperationException("Not implemented");
+	HeapFile file;
+	BucketScan scan;
+	HashIndex index;
+	boolean openflag;
+	
+	public IndexScan(Schema schema, HashIndex index, HeapFile file) {
+		this.schema=schema;
+		this.index=index;
+		scan=index.openScan();
+		openflag=true;
+	  
+	  // throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
@@ -21,35 +33,55 @@ public class IndexScan extends Iterator {
    * child iterators, and increases the indent depth along the way.
    */
   public void explain(int depth) {
-    throw new UnsupportedOperationException("Not implemented");
+	  indent(depth);
+		System.out.println("in the explain: " + file.toString());
+   // throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Restarts the iterator, i.e. as if it were just constructed.
    */
   public void restart() {
-    throw new UnsupportedOperationException("Not implemented");
+	  if (openflag) {
+			openflag = false;
+			scan.close();
+		}
+		// then we reopen the heapscan from heapscan
+		openflag = true;
+		scan = index.openScan();
+   // throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Returns true if the iterator is open; false otherwise.
    */
   public boolean isOpen() {
-    throw new UnsupportedOperationException("Not implemented");
+	  return openflag;
+	  //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Closes the iterator, releasing any resources (i.e. pinned pages).
    */
   public void close() {
-    throw new UnsupportedOperationException("Not implemented");
+		if (openflag) {
+			scan.close();
+			openflag = false;
+		} else {
+			System.out.println("there is no file need to be closed ");
+		}
+	  //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Returns true if there are more tuples, false otherwise.
    */
   public boolean hasNext() {
-    throw new UnsupportedOperationException("Not implemented");
+		if (openflag)
+			return scan.hasNext();
+		else
+			return false;
+   // throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
@@ -58,14 +90,26 @@ public class IndexScan extends Iterator {
    * @throws IllegalStateException if no more tuples
    */
   public Tuple getNext() {
-    throw new UnsupportedOperationException("Not implemented");
+	  if(openflag==false)
+		  throw new IllegalStateException("getnext error");
+	  try{
+		  RID  rid=scan.getNext();
+		  byte[] temp=file.selectRecord(rid);
+		  Tuple tuple =new Tuple(schema,temp);
+		  return tuple;
+	  }
+	  catch(Exception e){
+		  throw new IllegalStateException("getnext error:fail to get tuple");
+	  }
+	  //throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
    * Gets the key of the last tuple returned.
    */
   public SearchKey getLastKey() {
-    throw new UnsupportedOperationException("Not implemented");
+	  return scan.getLastKey();
+	  // throw new UnsupportedOperationException("Not implemented");
   }
 
   /**
@@ -73,7 +117,8 @@ public class IndexScan extends Iterator {
    * number of buckets if none.
    */
   public int getNextHash() {
-    throw new UnsupportedOperationException("Not implemented");
+    return scan.getNextHash();
+	  //throw new UnsupportedOperationException("Not implemented");
   }
 
 } // public class IndexScan extends Iterator
--- RelOperator/src/relop/KeyScan.java
+++ RelOperator/src/relop/KeyScan.java
@@ -1,64 +1,112 @@
 package relop;
 
+import global.RID;
 import global.SearchKey;
 import heap.HeapFile;
 import index.HashIndex;
+import index.HashScan;
 
 /**
  * Wrapper for hash scan, an index access method.
  */
 public class KeyScan extends Iterator {
 
-  /**
-   * Constructs an index scan, given the hash index and schema.
-   */
-  public KeyScan(Schema schema, HashIndex index, SearchKey key, HeapFile file) {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	HashScan scan;
+	HashIndex index;
+	HeapFile file;
+	SearchKey key;
+	boolean openflag;
 
-  /**
-   * Gives a one-line explaination of the iterator, repeats the call on any
-   * child iterators, and increases the indent depth along the way.
-   */
-  public void explain(int depth) {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Constructs an index scan, given the hash index and schema.
+	 */
+	public KeyScan(Schema schema, HashIndex index, SearchKey key, HeapFile file) {
+		this.index = index;
+		this.schema = schema;
+		this.key = key;
+		this.file = file;
+		scan = index.openScan(key);
+		openflag = true;
 
-  /**
-   * Restarts the iterator, i.e. as if it were just constructed.
-   */
-  public void restart() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+		// throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Returns true if the iterator is open; false otherwise.
-   */
-  public boolean isOpen() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Gives a one-line explaination of the iterator, repeats the call on any
+	 * child iterators, and increases the indent depth along the way.
+	 */
+	public void explain(int depth) {
+		indent(depth);
+		System.out.println("in the explain: " + file.toString());
+		// throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Closes the iterator, releasing any resources (i.e. pinned pages).
-   */
-  public void close() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Restarts the iterator, i.e. as if it were just constructed.
+	 */
+	public void restart() {
+		// check the openflag status
+		if (openflag) {
+			openflag = false;
+			scan.close();
+		}
+		// then we reopen the heapscan from heapscan
+		openflag = true;
+		scan = index.openScan(key);
+		// throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Returns true if there are more tuples, false otherwise.
-   */
-  public boolean hasNext() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Returns true if the iterator is open; false otherwise.
+	 */
+	public boolean isOpen() {
+		return openflag;
+		// throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Gets the next tuple in the iteration.
-   * 
-   * @throws IllegalStateException if no more tuples
-   */
-  public Tuple getNext() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Closes the iterator, releasing any resources (i.e. pinned pages).
+	 */
+	public void close() {
+		if (openflag) {
+			scan.close();
+			openflag = false;
+		} else {
+			System.out.println("there is no file need to be closed ");
+		}
+		// throw new UnsupportedOperationException("Not implemented");
+	}
+
+	/**
+	 * Returns true if there are more tuples, false otherwise.
+	 */
+	public boolean hasNext() {
+		if (openflag)
+			return scan.hasNext();
+		else
+			return false;
+		// throw new UnsupportedOperationException("Not implemented");
+	}
+
+	/**
+	 * Gets the next tuple in the iteration.
+	 * 
+	 * @throws IllegalStateException
+	 *             if no more tuples
+	 */
+	public Tuple getNext() {
+		  if(openflag==false)
+			  throw new IllegalStateException("getnext error");
+		  try{
+			  RID  rid=scan.getNext();
+			  byte[] temp=file.selectRecord(rid);
+			  Tuple tuple =new Tuple(schema,temp);
+			  return tuple;
+		  }
+		  catch(Exception e){
+			  throw new IllegalStateException("getnext error:fail to get tuple");
+		  }
+		//throw new UnsupportedOperationException("Not implemented");
+	}
 
 } // public class KeyScan extends Iterator
