dea04e6806ad537b40d78e9933bc8b99376dc582
chenyangqu@gmail.com <chenyangqu@gmail.com@27b30fb7-0727-6476-5e9b-203574951ffe>
2013-04-21 22:57:12 +0000

    implemented selection

--- trunk/RelOperator/src/relop/Selection.java
+++ trunk/RelOperator/src/relop/Selection.java
@@ -8,61 +8,103 @@ import externalSort.SortMergeJoin;
  * connected by OR operators.
  */
 public class Selection extends Iterator {
+	Iterator iterator;
+	Predicate[] predicate;
+	Tuple tuple;
 
-  /**
-   * Constructs a selection, given the underlying iterator and predicates.
-   */
-  public Selection(Iterator iter, Predicate... preds) {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Constructs a selection, given the underlying iterator and predicates.
+	 */
+	public Selection(Iterator iter, Predicate... preds) {
+		this.schema = iter.schema;
+		this.iterator = iter;
+		this.predicate = preds;
+		tuple = null;
+		// throw new UnsupportedOperationException("Not implemented");
+	}
 
-  public Selection(SortMergeJoin join2, Predicate predicate) {
-	// TODO Auto-generated constructor stub
-}
+	public Selection(SortMergeJoin join2, Predicate predicate) {
+		
+		// TODO Auto-generated constructor stub
+	}
 
-/**
-   * Gives a one-line explaination of the iterator, repeats the call on any
-   * child iterators, and increases the indent depth along the way.
-   */
-  public void explain(int depth) {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Gives a one-line explaination of the iterator, repeats the call on any
+	 * child iterators, and increases the indent depth along the way.
+	 */
+	public void explain(int depth) {
+			indent(depth);
+			System.out.println("Selection: "+predicate.toString());
+			depth++;
+			iterator.explain(depth);
+		//throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Restarts the iterator, i.e. as if it were just constructed.
-   */
-  public void restart() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Restarts the iterator, i.e. as if it were just constructed.
+	 */
+	public void restart() {
+		iterator.restart();
+		tuple=null;
+		//throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Returns true if the iterator is open; false otherwise.
-   */
-  public boolean isOpen() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Returns true if the iterator is open; false otherwise.
+	 */
+	public boolean isOpen() {
+		return iterator.isOpen();
+		//throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Closes the iterator, releasing any resources (i.e. pinned pages).
-   */
-  public void close() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Closes the iterator, releasing any resources (i.e. pinned pages).
+	 */
+	public void close() {
+		iterator.close();
+		tuple=null;
+		//throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Returns true if there are more tuples, false otherwise.
-   */
-  public boolean hasNext() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Returns true if there are more tuples, false otherwise.
+	 */
+	public boolean hasNext() {
+		boolean passflag=false;
+		if (isOpen())
+			return false;
+		else{
+			while(iterator.hasNext()){
+				boolean temp=false;
+				Tuple tp=iterator.getNext();
+				for(Predicate pre:predicate){
+					if(pre.evaluate(tp)){
+						passflag=true;
+						break;
+					}
+					if(passflag){
+						tuple=tp;
+						return true;
+					}
+				}
+			}return false;
+			
+		}
+		//throw new UnsupportedOperationException("Not implemented");
+	}
 
-  /**
-   * Gets the next tuple in the iteration.
-   * 
-   * @throws IllegalStateException if no more tuples
-   */
-  public Tuple getNext() {
-    throw new UnsupportedOperationException("Not implemented");
-  }
+	/**
+	 * Gets the next tuple in the iteration.
+	 * 
+	 * @throws IllegalStateException
+	 *             if no more tuples
+	 */
+	public Tuple getNext() {
+		if(tuple==null)
+			throw new IllegalStateException("getnext error");
+		else
+			return tuple;
+		//throw new UnsupportedOperationException("Not implemented");
+	}
 
 } // public class Selection extends Iterator
